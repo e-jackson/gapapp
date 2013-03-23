a4cb6648a68f21dc79ac7fd1ff20f2de500965ff
Lydia Damon <lydia.s.damon@gmail.com>
2013-03-23 16:20:51 -0400

    Set up Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nycgapapp::Application.config.secret_token = 'e88f4bfd2d3bcf9cd6a859d2c784fd192360bc00eadae9c15a674067f4b65e4189f403f43038f7c433ca9f9ba7ea53e270f27789f9fcdcad23a99b044361580f'
